e7df55abb947a7af544141c3a08db35578c49df6
Cndynajae <csagitasari@gmail.com>
2024-10-14 12:55:40 +0700

    Belajar CRUD Livewire

---
+++ app/Livewire/Post/Edit.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Livewire\Post;
+
+use App\Models\Post;
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public $postId;
+    public $title;
+    public $content;
+
+    public function mount($id)
+    {
+        $post = Post::find($id);
+
+        if($post) {
+            $this->postId   = $post->id;
+            $this->title    = $post->title;
+            $this->content  = $post->content;
+        }
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'title'   => 'required',
+            'content' => 'required',
+        ]);
+
+        if($this->postId) {
+
+            $post = Post::find($this->postId);
+
+            if($post) {
+                $post->update([
+                    'title'     => $this->title,
+                    'content'   => $this->content
+                ]);
+            }
+        }
+        //flash message
+        session()->flash('message', 'Data Berhasil Diupdate.');
+
+        //redirect
+        return redirect()->route('post.index');
+    }
+
+    public function render()
+    {
+        return view('livewire.post.edit');
+    }
+}
